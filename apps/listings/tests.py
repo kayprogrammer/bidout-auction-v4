85745e8ad9b4df1025886627d60ea559f1bf6e01
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-06-30 18:32:41 +0100

    retrieve listings tests

--- apps/listings/tests.py
+++ apps/listings/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from apps.accounts.auth import Authentication
+from apps.accounts.models import Jwt, Otp
 
-# Create your tests here.
+from apps.common.utils import TestUtil
+from unittest import mock
+
+
+class TestListings(APITestCase):
+    listings_url = "/api/v4/listings/"
+    listing_detail_url = "/api/v4/listings/detail/"
+    watchlist_url = "/api/v4/listings/watchlist/"
+    categories_url = "/api/v4/listings/categories/"
+    category_listings_url = "/api/v4/categories/"
+
+    def setUp(self):
+        verified_user = TestUtil.verified_user()
+        listing = TestUtil.create_listing(verified_user)
+
+    def test_retrieve_all_listings(self):
+        # Verify that all listings are retrieved successfully
+        response = self.client.get(self.listings_url)
+        self.assertEqual(response.status_code, 200)
+        result = response.json()
+        self.assertEqual(result["status"], "success")
+        self.assertEqual(result["message"], "Listings fetched")
+        data = result["data"]
+        self.assertGreater(len(data), 0)
+        self.assertTrue(any(isinstance(obj["name"], str) for obj in data))
