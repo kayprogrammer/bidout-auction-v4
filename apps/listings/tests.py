f76957ef0bb99e439b799b8a520c35c2db5a5c8d
Kenechi Ifeanyi <kayprogrammer1@gmail.com>
2023-06-30 18:57:03 +0100

    listing details test

--- apps/listings/tests.py
+++ apps/listings/tests.py
@@ -12,10 +12,11 @@ class TestListings(APITestCase):
     watchlist_url = "/api/v4/listings/watchlist/"
     categories_url = "/api/v4/listings/categories/"
     category_listings_url = "/api/v4/categories/"
+    maxDiff = None
 
     def setUp(self):
         verified_user = TestUtil.verified_user()
-        listing = TestUtil.create_listing(verified_user)
+        self.listing = TestUtil.create_listing(verified_user)["listing"]
 
     def test_retrieve_all_listings(self):
         # Verify that all listings are retrieved successfully
@@ -27,3 +28,45 @@ class TestListings(APITestCase):
         data = result["data"]
         self.assertGreater(len(data), 0)
         self.assertTrue(any(isinstance(obj["name"], str) for obj in data))
+
+    def test_retrieve_particular_listng(self):
+        listing = self.listing
+        # Verify that a particular listing retrieval fails with an invalid slug
+        response = self.client.get(f"{self.listing_detail_url}invalid_slug/")
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "failure",
+                "message": "Listing does not exist!",
+            },
+        )
+
+        # Verify that a particular listing is retrieved successfully
+        response = self.client.get(f"{self.listing_detail_url}{listing.slug}/")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {
+                "status": "success",
+                "message": "Listing details fetched",
+                "data": {
+                    "listing": {
+                        "auctioneer": mock.ANY,
+                        "name": listing.name,
+                        "slug": listing.slug,
+                        "desc": listing.desc,
+                        "category": "TestCategory",
+                        "price": mock.ANY,
+                        "closing_date": mock.ANY,
+                        "active": True,
+                        "bids_count": 0,
+                        "highest_bid": "0.00",
+                        "time_left_seconds": mock.ANY,
+                        "image": mock.ANY,
+                        "watchlist": False,
+                    },
+                    "related_listings": [],
+                },
+            },
+        )
